4c8218bcce5355805b5910780982ca5ea289160c
Omar <omar.branez@gmail.com>
2021-08-17 20:09:22 -0400

    commit tree 721fa9cda67dd8e4f47845c0818ed146e01c64f2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// find returns the first match
+// filter returns all matches as array
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
 
+function superbowlWin(arr) {
+    let winner = arr.find( row => row.result === "W" );
+    return ( !!winner ? winner.year : undefined ) 
+}
